cb2d44df3b84692cb1ad821d5dbb0032cd58d019
Oscar Teeninga <oscarteeninga@MacBook-Pro-16-Oscar.local>
2020-05-09 00:07:10 +0200

    Init Lab 4

---
+++ Lab4/custom_parser.py
@@ -0,0 +1,333 @@
+import scanner
+import ply.yacc as yacc
+import sys
+from structures import (Instructions,
+                        Assignment,
+                        BinaryExpression,
+                        LogicalExpression,
+                        MatrixExpression,
+                        Variable,
+                        Constant,
+                        Matrix,
+                        Vector,
+                        FunctionCall,
+                        PrintCall,
+                        IfCondition,
+                        ElseIfConditions,
+                        ElseIfCondition,
+                        ForLoop,
+                        ForCondition,
+                        WhileLoop,
+                        Break,
+                        Continue,
+                        Return
+                        )
+
+names = {}
+
+tokens = scanner.tokens
+
+precedence = ()
+
+
+def p_error(p):
+    if p:
+        print("Syntax error at line {0}, LexToken({1}, '{2}')".format(p.lineno, p.type, p.value))
+    else:
+        print("Unexpected end of input")
+
+
+def p_code(p):
+    """
+    CODE : INSTRUCTIONS
+    """
+    p[0] = p[1]
+
+
+def p_instructions(p):
+    """
+    INSTRUCTIONS : '{' INSTRUCTIONS '}'
+                | INSTRUCTION INSTRUCTIONS
+                | INSTRUCTION
+    """
+    if len(p) == 4:
+        p[0] = p[2]
+    elif len(p) == 3:
+        p[0] = Instructions(instructions=[p[1]]) + p[2]
+    else:
+        p[0] = Instructions(instructions=[p[1]])
+
+
+def p_instruction(p):
+    """
+    INSTRUCTION : ASSIGN ';'
+                | IF_CONDITION
+                | FOR_LOOP
+                | WHILE_LOOP
+                | CONTROL_INSTRUCTION ';'
+                | PRINT_CALL ';'
+    """
+    p[0] = p[1]
+
+
+def p_assign(p):
+    """
+    ASSIGN : ID ASSIGN_TYPE EXPRESSION
+           | ID '[' INTNUM ']' ASSIGN_TYPE EXPRESSION
+           | ID '[' INTNUM ',' INTNUM ']' ASSIGN_TYPE EXPRESSION
+    """
+    if len(p) == 4:
+        p[0] = Assignment(assignment_id=p[1], assignment_type=p[2], expression=p[3])
+    elif len(p) == 7:
+        p[0] = Assignment(assignment_id=p[1] + '[' + str(p[3]) + ']', assignment_type=p[5], expression=p[6])
+    else:
+        p[0] = Assignment(assignment_id=p[1] + '[' + str(p[3]) + ',' + str(p[5]) + ']', assignment_type=p[7],
+                          expression=p[8])
+
+
+def p_assign_type(p):
+    """
+    ASSIGN_TYPE : '='
+                | ADDASSIGN
+                | SUBASSIGN
+                | DIVASSIGN
+                | MULASSIGN
+    """
+    p[0] = p[1]
+
+
+def p_expression(p):
+    """
+    EXPRESSION : BINARY_EXPRESSION
+               | LOGICAL_EXPRESSION
+               | MATRIX_EXPRESSION
+               | '(' EXPRESSION ')'
+               | TERM
+    """
+    if len(p) == 2:
+        p[0] = p[1]
+    else:
+        p[0] = p[2]
+
+
+def p_binary_expression(p):
+    """
+    BINARY_EXPRESSION : EXPRESSION '+' EXPRESSION
+                      | EXPRESSION '-' EXPRESSION
+                      | EXPRESSION '*' EXPRESSION
+                      | EXPRESSION '/' EXPRESSION
+    """
+    p[0] = BinaryExpression(left=p[1], operation=p[2], right=p[3])
+
+
+def p_logical_expression(p):
+    """
+    LOGICAL_EXPRESSION : EXPRESSION '>' EXPRESSION
+                      | EXPRESSION '<' EXPRESSION
+                      | EXPRESSION EQORGT EXPRESSION
+                      | EXPRESSION EQORLESS EXPRESSION
+                      | EXPRESSION EQUAL EXPRESSION
+                      | EXPRESSION NOTEQUAL EXPRESSION
+    """
+    p[0] = LogicalExpression(left=p[1], operation=p[2], right=p[3])
+
+
+def p_matrix_expression(p):
+    """
+    MATRIX_EXPRESSION : EXPRESSION DOTADD EXPRESSION
+                      | EXPRESSION DOTSUB EXPRESSION
+                      | EXPRESSION DOTMUL EXPRESSION
+                      | EXPRESSION DOTDIV EXPRESSION
+    """
+    p[0] = MatrixExpression(left=p[1], operation=p[2], right=p[3])
+
+
+def p_term(p):
+    """
+    TERM : ID
+         | INTNUM
+         | FLOATNUM
+         | STRING
+         | MATRIX
+         | FUNCTION_CALL
+         | ID "'"
+         | '-' ID
+    """
+    if len(p) == 3:
+        p[0] = Variable(str(p[1]) + str(p[2]))
+    elif p.slice[1].type == 'ID':
+        p[0] = Variable(p[1])
+    elif p.slice[1].type == 'INTNUM':
+        p[0] = Constant(p[1])
+    elif p.slice[1].type == 'FLOATNUM':
+        p[0] = Constant(p[1])
+    elif p.slice[1].type == 'STRING':
+        p[0] = Constant(p[1])
+    else:
+        p[0] = p[1]
+
+def p_intnum_or_id(p):
+    """
+    INTNUM_OR_ID : INTNUM
+                 | ID
+    """
+    if p.slice[1].type == 'INTNUM':
+        p[0] = Constant(p[1])
+    else:
+        p[0] = Variable(p[1])
+
+
+def p_matrix(p):
+    """
+    MATRIX : '[' VECTORS ']'
+    """
+    p[0] = Matrix(p[2])
+
+
+def p_vectors(p):
+    """
+    VECTORS : VECTORS ';' VECTOR
+            | VECTOR
+    """
+    if len(p) == 4:
+        p[0] = p[1] + [p[3]]
+    else:
+        p[0] = [p[1]]
+
+
+def p_vector(p):
+    """
+    VECTOR : VECTOR ',' TERM
+           | TERM
+    """
+    if len(p) == 4:
+        p[0] = p[1] + [p[3]]
+    else:
+        p[0] = Vector(values=[p[1]])
+
+
+# Terms?
+def p_function_call(p):
+    """
+    FUNCTION_CALL : FUNCTION_NAME '(' TERM ')'
+    """
+    p[0] = FunctionCall(name=p[1], param=p[3])
+
+
+def p_function_name(p):
+    """
+    FUNCTION_NAME : ZEROS
+                  | ONES
+                  | EYE
+    """
+    p[0] = p[1]
+
+
+def p_control_instruction(p):
+    """
+    CONTROL_INSTRUCTION : BREAK
+                        | CONTINUE
+                        | RETURN
+                        | RETURN TERM
+    """
+    if len(p) == 3:
+        p[0] = Return(p[2])
+    elif p.slice[1].type == 'BREAK':
+        p[0] = Break()
+    elif p.slice[1].type == 'CONTINUE':
+        p[0] = Continue()
+    else:
+        p[0] = Return()
+
+
+def p_print_call(p):
+    """
+    PRINT_CALL : PRINT PRINT_TERMS
+    """
+    p[0] = p[2]
+
+
+def p_print_terms(p):
+    """
+    PRINT_TERMS : PRINT_TERMS ',' TERM
+                | TERM
+    """
+    if len(p) == 4:
+        p[0] = p[1] + PrintCall(values=[p[3]])
+    else:
+        p[0] = PrintCall(values=[p[1]])
+
+
+def p_if_condition(p):
+    """
+    IF_CONDITION    : IF '(' LOGICAL_EXPRESSION ')' INSTRUCTION ELIF_STATEMENTS ELSE_STATEMENT
+                    | IF '(' LOGICAL_EXPRESSION ')' '{' INSTRUCTIONS '}' ELIF_STATEMENTS ELSE_STATEMENT
+    ELIF_STATEMENTS : ELSEIF '(' LOGICAL_EXPRESSION ')' INSTRUCTION ELIF_STATEMENTS
+                    | ELSEIF '(' LOGICAL_EXPRESSION ')' '{' INSTRUCTIONS '}' ELIF_STATEMENTS
+                    | ELSEIF '(' LOGICAL_EXPRESSION ')' INSTRUCTION
+                    | ELSEIF '(' LOGICAL_EXPRESSION ')' '{' INSTRUCTIONS '}'
+    ELSE_STATEMENT  : ELSE INSTRUCTION
+                    | ELSE '{' INSTRUCTIONS '}'
+                    |
+    """
+    if p.slice[0].type == 'IF_CONDITION':
+        if len(p) == 8:
+            p[0] = IfCondition(condition=p[3], instructions=[p[5]], elif_branches=p[6], else_branch=p[7])
+        else:
+            p[0] = IfCondition(condition=p[3], instructions=[p[6]], elif_branches=p[8], else_branch=p[9])
+    elif p.slice[0].type == 'ELIF_STATEMENTS':
+        if len(p) == 6:
+            p[0] = ElseIfConditions(conditions=[ElseIfCondition(condition=p[3], instructions=p[5])])
+        if len(p) == 7:
+            p[0] = ElseIfConditions(conditions=[ElseIfCondition(condition=p[3], instructions=p[5])]) + p[6]
+        if len(p) == 8:
+            p[0] = ElseIfConditions(conditions=[ElseIfCondition(condition=p[3], instructions=p[6])])
+        elif len(p) == 9:
+            p[0] = ElseIfConditions(conditions=[ElseIfCondition(condition=p[3], instructions=p[5])]) + p[8]
+    elif p.slice[0].type == 'ELSE_STATEMENT':
+        if len(p) == 3:
+            p[0] = p[2]
+        elif len(p) == 5:
+            p[0] = p[3]
+
+
+def p_for_loop(p):
+    """
+    FOR_LOOP : FOR FOR_CONDITION '{' INSTRUCTIONS '}'
+             | FOR FOR_CONDITION  INSTRUCTION
+    """
+    if len(p) == 6:
+        p[0] = ForLoop(for_condition=p[2], instructions=p[4])
+    else:
+        p[0] = ForLoop(for_condition=p[2], instructions=p[3])
+
+
+def p_for_condition(p):
+    """
+    FOR_CONDITION : TERM '=' INTNUM_OR_ID ':' INTNUM_OR_ID
+    """
+    p[0] = ForCondition(variable=p[1], variable_range_first=p[3], variable_range_last=p[5])
+
+
+def p_while_loop(p):
+    """
+    WHILE_LOOP : WHILE '(' LOGICAL_EXPRESSION ')' '{' INSTRUCTIONS '}'
+               | WHILE '(' LOGICAL_EXPRESSION ')' INSTRUCTION
+    """
+    if len(p) == 8:
+        p[0] = WhileLoop(condition=p[3], instructions=p[6])
+    else:
+        p[0] = WhileLoop(condition=p[3], instructions=p[5])
+
+
+parser = yacc.yacc()
+file = open(sys.argv[1], "r")
+text = file.read()
+scanner.lexer.input(text)
+
+for token in scanner.lexer:
+    print("%d: %s(%s)" % (token.lineno, token.type, token.value))
+
+aaa = parser.parse(text, lexer=scanner.lexer)
+print("----")
+print(aaa)
---
+++ Lab4/structures.py
@@ -0,0 +1,195 @@
+class Node:
+    pass
+
+
+class Instructions:
+    def __init__(self, instructions):
+        self.instructions = instructions
+
+    def __add__(self, other):
+        self.instructions += other.instructions
+        return self
+
+    def __str__(self):
+        return "Instructions=(instructions=" + str([str(instruction) for instruction in self.instructions]) + ")"
+
+
+class Assignment:
+    def __init__(self, assignment_id, assignment_type, expression):
+        self.assignment_id = assignment_id
+        self.assignment_type = assignment_type
+        self.expression = expression
+
+    def __str__(self):
+        return "Assignment=(id=" + str(self.assignment_id) + ", type=" + self.assignment_type + ", expression=" + str(
+            self.expression) + ")"
+
+
+class BinaryExpression:
+    def __init__(self, left, operation, right):
+        self.left = left
+        self.operation = operation
+        self.right = right
+
+    def __str__(self):
+        return "BinaryExpression=(left=" + str(self.left) + ", operation=" + self.operation + ", right=" + str(
+            self.right) + ")"
+
+
+class LogicalExpression:
+    def __init__(self, left, operation, right):
+        self.left = left
+        self.operation = operation
+        self.right = right
+
+    def __str__(self):
+        return "LogicalExpression=(left=" + str(self.left) + ", operation=" + self.operation + ", right=" + str(
+            self.right) + ")"
+
+
+class MatrixExpression:
+    def __init__(self, left, operation, right):
+        self.left = left
+        self.operation = operation
+        self.right = right
+
+    def __str__(self):
+        return "MatrixExpression=(left=" + str(self.left) + ", operation=" + self.operation + ", right=" + str(
+            self.right) + ")"
+
+
+class Variable:
+    def __init__(self, variable_id):
+        self.variable_id = variable_id
+
+    def __str__(self):
+        return "Variable=(id=" + self.variable_id + ")"
+
+
+class Constant:
+    def __init__(self, const_value):
+        self.const_value = const_value
+
+    def __str__(self):
+        return "Constant=(id=" + str(self.const_value) + ")"
+
+
+class Matrix:
+    def __init__(self, vectors):
+        self.vectors = vectors
+
+    def __str__(self):
+        return "Matrix=(vectors=" + str([str(vector) for vector in self.vectors]) + ")"
+
+
+class Vector:
+    def __init__(self, values):
+        self.values = values
+
+    def __add__(self, other):
+        self.values += other
+        return self
+
+    def __str__(self):
+        return "Vector=(values=" + str([str(value) for value in self.values]) + ")"
+
+
+class FunctionCall:
+    def __init__(self, name, param):
+        self.name = name
+        self.param = param
+
+    def __str__(self):
+        return "FunctionCall=(name=" + self.name + ", param=" + str(self.param) + ")"
+
+
+class PrintCall:
+    def __init__(self, values):
+        self.values = values
+
+    def __add__(self, other):
+        self.values += other.values
+        return self
+
+    def __str__(self):
+        return "PrintCall=(values=" + str([str(value) for value in self.values]) + ")"
+
+
+class IfCondition:
+    def __init__(self, condition, instructions, elif_branches, else_branch):
+        self.condition = condition
+        self.instructions = instructions
+        self.elif_branches = elif_branches
+        self.else_branch = else_branch
+
+    def __str__(self):
+        return "IfCondition=(condition=" + str(self.condition) + ", instructions=" + str(
+            [str(instruction) for instruction in self.instructions]) + ", elif_branches=" + str(
+            [str(elif_branch) for elif_branch in self.elif_branches.conditions]) + ", else_branch=" + str(
+            self.else_branch) + ")"
+
+
+class ElseIfConditions:
+    def __init__(self, conditions):
+        self.conditions = conditions
+
+    def __add__(self, other):
+        self.conditions += other.conditions
+        return self
+
+    def __str__(self):
+        return "ElseIfConditions=(conditions=" + str([str(conditions) for conditions in self.conditions]) + ")"
+
+
+class ElseIfCondition:
+    def __init__(self, condition, instructions):
+        self.condition = condition
+        self.instructions = instructions
+
+    def __str__(self):
+        return "ElseIfCondition=(condition=" + str(self.condition) + ", instructions=" + str(self.instructions) + ")"
+
+
+class ForLoop:
+    def __init__(self, for_condition, instructions):
+        self.for_condition = for_condition
+        self.instructions = instructions
+
+    def __str__(self):
+        return "ForLoop=(for_condition=" + str(self.for_condition) + ", instructions=" + str(self.instructions) + ")"
+
+
+class ForCondition:
+    def __init__(self, variable, variable_range_first, variable_range_last):
+        self.variable = variable
+        self.variable_range_first = variable_range_first
+        self.variable_range_last = variable_range_last
+
+    def __str__(self):
+        return "ForCondition=(variable=" + str(self.variable) + ", range=" + str(self.variable_range_first) + ":" + str(
+            self.variable_range_last) + ")"
+
+
+class WhileLoop:
+    def __init__(self, condition, instructions):
+        self.condition = condition
+        self.instructions = instructions
+
+    def __str__(self):
+        return "WhileLoop=(condition=" + str(self.condition) + ", instructions=" + str(self.instructions) + ")"
+
+class Break:
+    def __str__(self):
+        return "Break=()"
+
+class Continue:
+    def __str__(self):
+        return "Continue=()"
+
+
+class Return:
+    def __init__(self, instruction=None):
+        self.instruction = instruction
+
+    def __str__(self):
+        return "Return=(term=(" + str(self.instruction) + "))"
